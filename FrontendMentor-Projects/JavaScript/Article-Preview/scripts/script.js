d31823ef72a1ffdb3f92079dcfb4c5addde31b70
Kyle <createkyle@protonmail.com>
2025-04-08 08:31:40 -0400

    Update of 4/7
    - FEM Article Preview
    -- Added close button to share-container-icons for mobile view
    -- Added comments to JS file
    -- Re-wrote CSS starting with mobile first in mind and then desktop

--- FrontendMentor-Projects/JavaScript/Article-Preview/scripts/script.js
+++ FrontendMentor-Projects/JavaScript/Article-Preview/scripts/script.js
@@ -1,10 +1,14 @@
 "use strict";
 
 function toggleShareIcons() {
+    // Icons container ID
     const shareIcons = document.getElementById('shareIcons');
 
+    // If visibile, hide
     if (shareIcons.classList.contains('visible')) {
         shareIcons.classList.remove('visible');
+
+    // If hidden, show
     } else {
         shareIcons.classList.add('visible');
     }
